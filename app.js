c3873a9a95fa0a2e36f2be0d71c1d5fecd688f88
sm2101 <sidarthmittal5@gmail.com>
2020-03-16 17:37:11 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var q = Math.floor(Math.random() * 3); 
+    var s = ["images/fsociety.png","images/geass.png","images/aw-1.png","images/westworld.png"]
+    $("#imgdis").attr("src",s[q]);
+    $("#next").click(function(){
+        if(q == s.length-1){
+            q = -1
+        }
+        q++;
+        $("#imgdis").attr("src",s[q]);
+    })
+    $("#prev").click(function(){
+        if(q == 0){
+            q = s.length
+        }
+        q--;
+        $("#imgdis").attr("src",s[q]);
+    })
+})
