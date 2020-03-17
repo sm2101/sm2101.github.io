0c92ff3f9e04aa91f88b325960f80b260c737b07
sm2101 <51268516+sm2101@users.noreply.github.com>
2020-03-18 00:57:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var q = Math.floor(Math.random() * 3); 
-    var s = ["images/fsociety.png","images/geass.png","images/aw-1.png","images/westworld.png"]
+    var s = ["images/fsociety.png","images/geass.png","images/aw-1.png","images/westworld.png","images/L-1.png","images/light.png"]
     $("#imgdis").attr("src",s[q]);
     $("#next").click(function(){
         if(q == s.length-1){
